12c143b0e1888df3d26f7b6f9b636091b5d0f8c2
Legolas Bloom <huan.zhan@shanbay.com>
2016-10-10 22:17:49 +0800

    add sh for init ubuntu vim

---
+++ README.md
@@ -0,0 +1,16 @@
+# legolas-vim
+个人vim配置。参考帖子：[vim与Python真乃天作之合](http://codingpy.com/article/vim-and-python-match-in-heaven/)
+
+支持Python自动补全的最好插件是YouCompleteMe。而YouCompleteMe需要比较高版本的vim，所以，需要编译安装vim，参见相关文档[1]。另外还需要一些C库，参见相关文档[2]。
+
+vim扩展管理器使用的是Vundle，参见相关文档[3]。
+
+以上部分使用各平台脚本可自动安装。例如，在ubuntu系统上，运行以下命令即可：
+
+    sudo bash init-vim-ubuntu.sh
+
+# 相关文档
+
+1. [Building-Vim-from-source](https://github.com/Valloric/YouCompleteMe/wiki/Building-Vim-from-source)
+2. [https://github.com/Valloric/YouCompleteMe](https://github.com/Valloric/YouCompleteMe)
+3. [https://github.com/VundleVim/Vundle.vim](https://github.com/VundleVim/Vundle.vim)
---
+++ init-vim-ubuntu.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# Init vim in Ubuntu
+function echoo {
+    echo -e "\n\033[32m $1 \033[0m"
+}
+
+# backup
+echoo ">>> Backing up old vim configs"
+
+today=`date +%Y%m%d`
+for i in $HOME/.vim $HOME/.vimrc; do 
+    [ -e $i ] && mv $i $i.$today; 
+done
+
+
+# clone vim
+echoo ">>> Building Vim from source: https://github.com/Valloric/YouCompleteMe/wiki/Building-Vim-from-source"
+
+echoo ">>> First, install all the prerequisite libraries, including Git..."
+sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
+    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
+    python3-dev build-essential cmake git
+
+echoo ">>> Remove vim if you have it already..."
+sudo apt-get remove vim vim-runtime gvim
+
+echoo ">>> install vim..."
+cd ~ && rm -rf vim && git clone https://github.com/vim/vim.git && cd vim
+./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-pythoninterp \
+            --enable-python3interp \
+            --with-mac-arch=x86_64 \
+            --enable-gui=gtk2 --enable-cscope --prefix=/usr
+make VIMRUNTIMEDIR=/usr/share/vim/vim74
+sudo make install
+
+echoo ">>> Set up Vundle..."
+sudo rm -rf ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echoo "SUCCESS"
