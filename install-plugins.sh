54f81710ecbbcc8d78d64b7af7235b6ae2a853ec
Legolas Bloom <huan.zhan@shanbay.com>
2016-10-11 20:28:33 +0800

    finish vim-flake8 and use F7 to check py file

--- install-plugins.sh
+++ install-plugins.sh
@@ -2,10 +2,12 @@
 
 function update {
     cp vimrc $HOME/.vimrc
+    sudo vim +PluginClean +qall
     sudo vim +PluginInstall +qall
 }
 
 function init {
+    pip install flake8  # for vim-flake8
     update;
     cd ~/.vim/bundle/YouCompleteMe && sudo ./install.py --clang-completer
 }
