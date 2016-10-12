5a57b0fd1f1e551cc1716cd66c44640b49d1251c
Legolas Bloom <zhanhsw@gmail.com>
2016-10-12 14:37:31 +0800

    fix cmake bug and enhance install tips

--- init-vim-ubuntu.sh
+++ init-vim-ubuntu.sh
@@ -16,16 +16,16 @@ done
 # clone vim
 echoo ">>> Building Vim from source: https://github.com/Valloric/YouCompleteMe/wiki/Building-Vim-from-source"
 
-echoo ">>> First, install all the prerequisite libraries, including Git..."
+echoo ">>> First, install all the prerequisite libraries, including Git ..."
 sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
     libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
     libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
     python3-dev build-essential cmake git
 
-echoo ">>> Remove vim if you have it already..."
+echoo ">>> Remove vim if you have it already ..."
 sudo apt-get remove vim vim-runtime gvim
 
-echoo ">>> install vim..."
+echoo ">>> Install vim ..."
 cd ~ && rm -rf vim && git clone https://github.com/vim/vim.git && cd vim
 ./configure --with-features=huge \
             --enable-multibyte \
@@ -38,7 +38,7 @@ cd ~ && rm -rf vim && git clone https://github.com/vim/vim.git && cd vim
 make VIMRUNTIMEDIR=/usr/share/vim/vim80
 sudo make install
 
-echoo ">>> Set up Vundle..."
+echoo ">>> Set up Vundle ..."
 sudo rm -rf ~/.vim/bundle/Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
