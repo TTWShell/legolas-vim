86c4f3cedb5b8651b9f852d1aa8c5321c87641c0
Legolas Bloom <zhanhsw@gmail.com>
2017-01-19 00:08:25 +0800

    fixed backup bug in install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
-
 function echoo {
     echo -e "\n\033[32m $1 \033[0m"
 }
 
+function backup {
+    echoo ">>> Backing up old vim configs"
 
-# backup
-echoo ">>> Backing up old vim configs"
-
-today=`date +%Y%m%d`
-for i in $HOME/.vim $HOME/.vimrc; do
-    [ -e $i ] && sudo mv $i $i.$today;
-done
-
+    today=`date +%Y%m%d`
+    for i in $HOME/.vim $HOME/.vimrc; do
+        [ -e $i ] && sudo mv $i $i.$today;
+    done
+}
 
 platform=""
 for i in "Centos","Centos" "Ubuntu","Ubuntu" "Darwin","MacOSX"; do
@@ -28,6 +26,7 @@ done
 read -p "Rebulid the vim and all plugins, Are you sure(Y/N)? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
+    backup
     case $platform in
         Centos)
             sudo bash scripts/init-vim-centos.sh
@@ -43,5 +42,5 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     sudo bash install-plugins.sh init
     echoo "Install finished."
 else
-    echoo "The user cancels the operation."
+    echoo "User cancels the operation."
 fi
