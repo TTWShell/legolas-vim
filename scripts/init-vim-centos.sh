f7e91fcc402e94481480c91a62860febd572933c
Legolas Bloom <zhanhsw@gmail.com>
2017-02-11 15:27:58 +0800

    build install vim 8

--- scripts/init-vim-centos.sh
+++ scripts/init-vim-centos.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-
 # for ycm
-yum install gcc gcc-c++ kernel-devel cmake golang
+yum install -y gcc gcc-c++ kernel-devel cmake golang
 
+sudo yum -y remove vim
 
-# for tarlist
-yum install ctags
+cd ~ && rm -rf vim && git clone git@github.com:vim/vim.git && cd vim
+./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-pythoninterp \
+	    #--with-python-config-dir=/usr/lib/python2.7/config \
+            --enable-python3interp \
+            #--with-python3-config-dir=/usr/lib/python3.4/config \
+            --with-mac-arch=x86_64 \
+            --enable-gui=gtk2 --enable-cscope --prefix=/usr
+make VIMRUNTIMEDIR=/usr/local/share/vim/vim80
+sudo make install
