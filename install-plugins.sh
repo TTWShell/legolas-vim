f3f0b333e9eab232d9756fad3d571f3ec8f1adc9
Legolas Bloom <zhanhsw@gmail.com>
2017-03-28 13:13:45 +0800

    enhance install-plugins.sh & add cmd for rebuild ycm

--- install-plugins.sh
+++ install-plugins.sh
@@ -21,14 +21,41 @@ function init {
 
     sudo mkdir -p ~/.vim/colors && sudo cp ~/.vim/bundle/vim-colorschemes/colors/* ~/.vim/colors
 
+    rebuild
+}
+
+function rebuild {
     cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive && sudo ./install.py --clang-completer --gocode-completer
 }
 
-if [[ $1 = "init" ]]; then
-    init
-elif [[ $1 = "update" ]]; then
-    install
-    update
-else
-    install
-fi
+function usage {
+    printf "Usage:\n"
+    printf "    init\t初始化安装插件\n"
+    printf "    install\t安装新增变更配置\n"
+    printf "    update\t更新配置且更新所有插件。可能会导致ycm不能正常工作，此时需要执行 rebuild 命令\n"
+    printf "    rebuild\trebuild YouCompleteMe\n"
+}
+
+
+Action=$1
+shift
+case "$Action" in
+    init)
+        init "$@"
+        ;;
+    update)
+        install
+        update
+        ;;
+    install)
+        install
+        ;;
+    rebuild)
+        rebuild "$@"
+        ;;
+    *)
+        usage
+        exit 1
+        ;;
+esac
+exit 0
